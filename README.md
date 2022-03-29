a1e3c3d990c04529c2c201521aeea10c7791a2c2
raw-vitor <vitorgomesds1@gmail.com>
2022-03-29 00:01:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-nmpmbf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-nmpmbf)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'typeface-roboto';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Estamos codando há 0 dias sem bugs no deploy</h1>
+      <h1>Nosso recorde foi de 0 dias.</h1>
+    </div>
+  );
+}
+
+export default App;
